173dc235a2b3d3c5d42ddd0ffe3af0202e8a8e89
lichtle <leticia.negreiros@outlook.com>
2023-11-07 13:31:28 -0300

    chore: projeto finalizado

---
+++ src/components/Formulario.vue
@@ -0,0 +1,36 @@
+<script setup>
+const props = defineProps([
+  "cadastrarTarefa",
+  "tarefaASerCadastrada",
+  "filtro",
+  "editarTarefaASerCadastrada",
+]);
+</script>
+
+<template>
+  <form @submit.prevent="props.cadastrarTarefa">
+    <!-- A função preventDefault() pode ser substituída por um simples "prevent" após o nome do evento (submit) -->
+    <div class="row">
+      <div class="col">
+        <input
+          type="text"
+          required
+          :value="props.tarefaASerCadastrada"
+          @change="props.editarTarefaASerCadastrada"
+          class="form-control"
+          placeholder="Escreva uma nova tarefa"
+        />
+      </div>
+      <div class="col-md-1">
+        <button type="submit" class="btn btn-primary">Adicionar</button>
+      </div>
+      <div class="col-md-2">
+        <select class="form-control" @change="props.filtro">
+          <option value="todas">Todas as tarefas</option>
+          <option value="pendentes">Tarefas pendentes</option>
+          <option value="finalizadas">Tarefas concluídas</option>
+        </select>
+      </div>
+    </div>
+  </form>
+</template>
---
+++ src/components/ListaDeTarefas.vue
@@ -0,0 +1,30 @@
+<script setup>
+const props = defineProps(["tarefas"]);
+</script>
+
+<template>
+  <p class="mt-4" v-if="tarefas.length < 1">
+    Você ainda não possui nenhuma tarefa cadastrada.
+  </p>
+  <ul class="list-group mt-4" v-else>
+    <li class="list-group-item" v-for="tarefa in props.tarefas">
+      <input
+        type="checkbox"
+        @change="(evento) => (tarefa.finalizada = evento.target.checked)"
+        :checked="tarefa.finalizada"
+        :id="tarefa.titulo"
+      /><label
+        :for="tarefa.titulo"
+        class="ms-2"
+        :class="{ done: tarefa.finalizada === true }"
+        >{{ tarefa.titulo }}</label
+      >
+    </li>
+  </ul>
+</template>
+
+<style scoped>
+.done {
+  text-decoration: line-through;
+}
+</style>
